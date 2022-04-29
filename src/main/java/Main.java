ef1786b39fee2ea8286ca6dc792b994c0a55e63e
Evgeniy Cherkasov <echerkasov@fxdd.com>
2022-04-29 08:37:10 -0700

    run()

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,10 +22,36 @@ public class Main {
 
         giena.nameDanger();
         feed(giena);
+
+        Animal animal1 = getAnimal1();
+        Animal animal2 = getAnimal1();
+        Animal animal3 = getAnimal1();
+        Animal animal4 = getAnimal1();
+
+        animal1.nameDanger();
+        animal2.nameDanger();
+        animal3.nameDanger();
+        animal4.nameDanger();
     }
 
     //METOD FOR FEED
     private static void feed(Animal pers) {
         System.out.printf(" feed - %s %n %n", pers.name);
+    }
+
+    private static Animal getAnimal1() {
+        return new Dog();
+    }
+
+    private static Animal getAnimal2() {
+        return new Giena();
+    }
+
+    private static Animal getAnimal3() {
+        return new Shakal();
+    }
+
+    private static Animal getAnimal4() {
+        return new Wolf();
     }
 }
